237653bb72620a568bb745151d60a3877eb65fd4
Almashka <almashkakz@mail.ru>
2017-02-13 14:26:11 -0600

    Prime Numbers from 1 to 100: java code added

---
+++ PrimeNumbers.java
@@ -0,0 +1,25 @@
+
+public class PrimeNumbers {
+    
+    public static void main(String[] args) {
+        int i, num;
+        //Empty String
+        String  primeNumbers = "";
+        
+        for (i = 1; i <= 100; i++) {
+            int counter = 0;
+            
+            for(num = i; num >= 1; num--) {
+                if(i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+        //Appended the Prime number to the String
+        primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Prime numbers from 1 to 100 are :");
+        System.out.println(primeNumbers);
+    }
+}
